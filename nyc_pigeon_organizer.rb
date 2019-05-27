b67cac2181d85bff00a05f70facba6987739ea9c
beatific-revision-5216 <ivanov_chris@yahoo.com>
2019-05-27 00:37:27 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -72,7 +72,7 @@ def form(name, data)
 end
 
 def nyc_pigeon_organizer(data)
-  p data[:gender][:male].concat(data[:gender][:female])).collect { |pigeon| form(pigeon, data) }
+  p data[:gender][:male].concat(data[:gender][:female]).collect { |pigeon| form(pigeon, data) }
 end
 
 nyc_pigeon_organizer pigeon_data
