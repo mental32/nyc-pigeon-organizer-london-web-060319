4a082144377a94c6d2bf2baff1e52661254f4b3a
beatific-revision-5216 <ivanov_chris@yahoo.com>
2019-05-27 00:48:29 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,15 @@
+def find(key, name, data)
+  data[key].select { |_, v| v.include? name }
+end
+
+def form(name, data, gender)
+  {
+      color: find(:color, name, data).keys.map { |k| k.to_s },
+      gender: [gender.to_s],
+      lives: [find(:lives, name, data).keys.first]
+  }
+end
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  Hash[ *data[:gender].map { |gender, arr| arr.collect { |pigeon| [pigeon, form(pigeon, data, gender)] } }.flatten ]
 end
