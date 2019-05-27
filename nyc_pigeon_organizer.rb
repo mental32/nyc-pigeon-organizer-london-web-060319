d247b58c34b4d36bbbffdd89475cba0301922e1e
beatific-revision-5216 <ivanov_chris@yahoo.com>
2019-05-27 00:34:44 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -72,7 +72,7 @@ def form(name, data)
 end
 
 def nyc_pigeon_organizer(data)
-  Hash[ *(data[:gender][:male].concat(data[:gender][:female])).collect { |pigeon| form pigeon }.flatten ]
+  Hash[ *(data[:gender][:male].concat(data[:gender][:female])).collect { |pigeon| form(pigeon, data) }.flatten ]
 end
 
 nyc_pigeon_organizer pigeon_data
