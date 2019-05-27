357846de92bb9c4bdf145be8291fc01eeca0929c
beatific-revision-5216 <ivanov_chris@yahoo.com>
2019-05-27 00:27:11 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,80 @@
+pigeon_data = {  :color => {
+    :purple => ["Theo", "Peter Jr.", "Lucky"],
+    :grey => ["Theo", "Peter Jr.", "Ms. K"],
+    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+    :brown => ["Queenie", "Alex"]
+  },
+  :gender => {
+    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+    :female => ["Queenie", "Ms. K"]
+  },
+  :lives => {
+    "Subway" => ["Theo", "Queenie"],
+    "Central Park" => ["Alex", "Ms. K", "Lucky"],
+    "Library" => ["Peter Jr."],
+    "City Hall" => ["Andrew"]
+  }
+}
+
+# Iterate over the hash above, collecting each pigeon by name and insert it
+# as the key of a new hash where each name holds the attributes for that bird. 
+# Your output should look something like the hash below:
+
+
+# pigeon_list = {
+#   "Theo" => {
+#     :color => ["purple", "grey"],
+#     :gender => ["male"],
+#     :lives => ["Subway"]
+#   },
+#   "Peter Jr." => {
+#     :color => ["purple", "grey"],
+#     :gender => ["male"],
+#     :lives => ["Library"]
+#   },
+#   "Lucky" => {
+#     :color => ["purple"],
+#     :gender => ["male"],
+#     :lives => ["Central Park"]
+#   },
+#   "Ms. K" => {
+#     :color => ["grey", "white"],
+#     :gender => ["female"],
+#     :lives => ["Central Park"]
+#   },
+#   "Queenie" => {
+#     :color => ["white", "brown"],
+#     :gender => ["female"],
+#     :lives => ["Subway"]
+#   },
+#   "Andrew" => {
+#     :color => ["white"],
+#     :gender => ["male"],
+#     :lives => ["City Hall"]
+#   },
+#   "Alex" => {
+#     :color => ["white", "brown"],
+#     :gender => ["male"],
+#     :lives => ["Central Park"]
+#   }
+# }
+
+
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  find =
+
+  def form(name)
+
+
+    {
+      color: [find(:color).keys.map { |k| k.to_s }],
+      gender: [find(:gender).keys.first.to_s],
+      lives: [find(:lives).keys.first]
+    }
+  end
+
+  Hash[ *(data[:gender][:male].concat(data[:gender][:female])).collect { |pigeon| form(pigeon) }.flatten ]
 end
+
+nyc_pigeon_organizer pigeon_data
